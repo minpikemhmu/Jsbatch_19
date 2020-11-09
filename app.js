d2e320c0ce7f5dfc68f5e3f1af65f4df6f6f657b
minpikemhmu <minpikehmu10@gmail.com>
2020-11-09 11:22:14 +0630

    first lesson

---
+++ app.js
@@ -0,0 +1,96 @@
+var greet="Hello Bootcampers";
+//console.log(greet.length);
+
+var myarray=["Jan","Feb","March"];
+//console.log(myarray.length);
+
+var str="Welcome to Myanmar IT Consulting";
+var pos=str.search("xyz");
+//console.log(pos);
+
+//search
+//shi nay yin position 
+//-1
+
+//slice()
+/*var str="Apple, Banana, Kiwi";
+var res=str.slice(-19,-13);
+console.log(res);*/
+
+//substring()
+/*var str="Apple, Banana, Kiwi";
+var res=str.substring(7);
+console.log(res);*/
+
+//replace()
+/*var str="Welcome Boootcampers";
+var res=str.replace("Welcome","Hello");
+console.log(res);*/
+
+//Number methods
+//let x=123;
+//tostring();
+/*var y=x.toString();
+console.log(typeof y);*/
+
+//tofixed()
+/*var x=9.656;
+console.log(x.toFixed(0));*/
+
+//Number
+/*var x=false;
+console.log(Number(x));*/
+
+//parseInt()
+/*var x=10.6789;
+console.log(parseInt("10 years"))*/
+
+
+//Array Method
+/*var months=["Feb","Jan","March"];
+var stringmonth=months.toString();
+console.log(typeof stringmonth);*/
+
+//join();
+/*var months=["Feb","Jan","March"];
+var stringmonth=months.join("*");
+console.log(stringmonth);*/
+
+//push() and pop();
+var months=["Jan","Feb","March","April","May","June","July"];
+var last=months[months.length-1];
+console.log(last);
+
+/*months.push("April");
+console.log(months);
+
+months.pop();
+console.log(months);
+
+months.shift();
+console.log(months);
+
+months.unshift("Jan");
+console.log(months);
+
+console.log(months[0]);
+months[0]="January";
+console.log(months);*/
+
+//splice();
+/*months.splice(1,1,"May","Mibn","mmmm");
+console.log(months);*/
+
+/*var array=["May","June","July"];
+var myarray=months.concat(array);
+console.log(myarray);*/
+
+var person={firstname:"John",lastname:"Doe"};
+console.log(person.firstname);
+
+
+//asssoc
+//object
+//operateors
+//conditional statement
+//looping
